eb9f4182d9f23976e056d7935a2e4f51a7a26423
blurk <nsinh6745@gmail.com>
2020-06-28 00:05:59 +0700

    Updates

---
+++ precache-manifest.91d23cc6cd45eb4694dfd56667bfc136.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486e13e9f9a6d262e914816d76e68c91",
+    "url": "/corona_tracker/index.html"
+  },
+  {
+    "revision": "e96ff940a91d9a2de57d",
+    "url": "/corona_tracker/static/css/main.7e737197.chunk.css"
+  },
+  {
+    "revision": "ce46ac343c08a30af1ba",
+    "url": "/corona_tracker/static/js/2.5ebe4d38.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona_tracker/static/js/2.5ebe4d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e96ff940a91d9a2de57d",
+    "url": "/corona_tracker/static/js/main.34df0e1c.chunk.js"
+  },
+  {
+    "revision": "5a00df15640bdb1f13d7",
+    "url": "/corona_tracker/static/js/runtime-main.4dc9f99a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona_tracker/static/media/image.d7265326.png"
+  }
+]);
